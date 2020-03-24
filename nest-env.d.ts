c00676dd573fff83982e0f41930a3991c7353d10
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2020-03-24 11:42:29 -0600

    feat: Setup mongoose
    Use env variable to configure the connection URI
    Use docker compose to run mongo

--- nest-env.d.ts
+++ nest-env.d.ts
@@ -2,5 +2,6 @@ declare namespace NodeJS {
   export interface ProcessEnv {
     readonly NODE_ENV: 'development' | 'test' | 'production';
     readonly PORT: string;
+    readonly MONGODB_URI: string;
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
