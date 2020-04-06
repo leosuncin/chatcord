7147f2722aae9c4eda7570406290ff851a042c8b
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2020-04-05 20:58:50 -0600

    feat(mail): Setup nodemailer integration
    Patch @nestjs-modules/mailer to remove error

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
+import { MailerModule, MailerOptions } from '@nestjs-modules/mailer';
 import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { createTestAccount } from 'nodemailer';
 import { join } from 'path';
 
 import { AuthModule } from '~auth/auth.module';
@@ -20,6 +22,21 @@ import { ChatModule } from '~chat/chat.module';
     BullModule.registerQueue({
       name: 'email',
       redis: process.env.REDIS_URL,
+    }),
+    MailerModule.forRootAsync({
+      useFactory: async (): Promise<MailerOptions> => {
+        const account = await createTestAccount();
+
+        return {
+          transport: {
+            ...account.smtp,
+            auth: {
+              user: account.user,
+              pass: account.pass,
+            },
+          },
+        };
+      },
     }),
     ChatModule,
     AuthModule,
